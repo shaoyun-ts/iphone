6ec2bff83c0731b605d8dcdc3879aa4c7c6529ce
shaoyun-ts <56466207@qq.com>
2016-03-07 23:48:31 +0800

    2016-3-7

--- iphone/ViewController.swift
+++ iphone/ViewController.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIAlertViewDelegate{
 
+    
     @IBOutlet weak var Label1: UILabel!
     @IBAction func BT1(sender: AnyObject) {
         Label1.text = ("我爱 Swift !")
-    }
+        let tip1 = UIAlertController(title: "选择提示框", message: "确认还是取消？", preferredStyle: .Alert)
+        let OK1 = UIAlertAction(title: "确认", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+             self.Label1.text = ("你选择了确认")
+        }
+        let ca1 = UIAlertAction(title: "取消", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
+             self.Label1.text = ("你选择了取消")
+        }
+        tip1.addAction(OK1)
+        tip1.addAction(ca1)
+            self.presentViewController(tip1, animated: true, completion: nil )
+        }
+       
+            }
+
+
   
-        override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
-}
 
